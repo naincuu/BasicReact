73fc1bd620e407647f5d70ebe439620a2a97f000
naincuu <156917110+naincuu@users.noreply.github.com>
2024-03-02 13:41:52 +0500

    Add files via upload

---
+++ MyFirstVietProject/src/App.jsx
@@ -0,0 +1,54 @@
+
+import './App.css';
+
+
+function App() {
+
+  return (
+    <>
+      <h1> This is  my First  React Project  I am learning this basic Code From Chai Or Code</h1>
+      <br/>
+      <lable> FirstName </lable>
+      <br/>
+      <input type='text'></input>
+
+      <br/>
+      <lable> Email </lable>
+      <br/>
+      <input type='email'></input>
+
+      <br/>
+      
+      <lable> Pass </lable>
+      <br/>
+      <input type='password'></input>
+      <br/>
+      <br/>
+      <br/>
+      <button>Submit</button>
+    </>
+  )
+}
+
+export default App;
+
+
+
+// function App()
+// {
+//     return  (
+//         <>
+//         <h1> Hey  My name is Nancy Kingrani I am react Developer</h1>
+//         </>
+//     )
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
---
+++ MyFirstVietProject/src/main.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+    <App />
+  // </React.StrictMode>,
+
+)
+
+
+// import React from 'react';
+// import ReactDOM from   'react-dom/client';
+// import App from "./App.jsx";
+
+// ReactDOM .createRoot(document.getElementById("root")).render(
+//     <App/>
+
+// )
+
+
+
+
+// import Person from './anothoer/First';
+// function App()
+// {
+//     return (
+//         <div className='App'>
+//             <Person/>
+//         </div>
+            
+//     );
+// }
+
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
